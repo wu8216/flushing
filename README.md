194f22caaa17d017805b56221c132e06c38aacb4
wu8216 <111179295+wu8216@users.noreply.github.com>
2024-11-15 14:11:35 -0500

    Add files via upload
    Initial checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+# streamlit_app_gallery
+This is a multi-page streamlit app to showcase Sharone's streamlit app examples
---
+++ streamlit_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.markdown("## New to the Market! asking $2,380,000", unsafe_allow_html=True)
+
+st.image("media/A00.jpg", caption="Summary")
+st.image("media/A01.jpg", caption="Exterior Front")
+st.image("media/A02.jpg", caption="Living Room & Dining Area")
+st.image("media/A03.jpg", caption="Living Room & Dining Area")
+st.image("media/A04.jpg", caption="Kitchen")
+st.image("media/A05.jpg", caption="Bedroom")
+st.image("media/A06.jpg", caption="Bedroom")
+st.image("media/A07.jpg", caption="Bedroom")
+st.image("media/A08.jpg", caption="Bedroom")
+st.image("media/A09.jpg", caption="Bathroom")
+st.image("media/A10.jpg", caption="Bathroom")
+st.image("media/A11.jpg", caption="Laundry Closet")
+st.image("media/A12.jpg", caption="Balcony")
+st.image("media/A13.jpg", caption="Balcony")
+st.image("media/A14.jpg", caption="Balcony View")
+st.image("media/A15.jpg", caption="Living Room & Dining Area")
+st.image("media/A16.jpg", caption="Living Room & Dining Area")
+st.image("media/A17.jpg", caption="Laundry Room")
+st.image("media/A18.jpg", caption="Bedroom")
+st.image("media/A19.jpg", caption="Bathroom")
+st.image("media/A20.jpg", caption="Living Room")
+st.image("media/A21.jpg", caption="Kitchen")
+st.image("media/A22.jpg", caption="Bedroom")
+st.image("media/A23.jpg", caption="Bedroom")
+st.image("media/A24.jpg", caption="Bedroom")
+st.image("media/A25.jpg", caption="Bathroom")
+st.image("media/A26.jpg", caption="Bathroom")
+st.image("media/A27.jpg", caption="Deck")
+st.image("media/A28.jpg", caption="Backyard")
+st.image("media/A29.jpg", caption="Backyard")
+st.image("media/A30.jpg", caption="Dock")
+st.image("media/A31.jpg", caption="Dock")
+st.image("media/A32.jpg", caption="Exterior Front")
+st.image("media/A33.jpg", caption="Exterior Front")
+
+
+song_html=''' <iframe width="1011" height="569" src="https://www.youtube.com/embed/QCL7VXuO35g" title="24 HOURS of 4K Underwater Wonders + Relaxing Music - The Best 4K Sea Animals for Relaxation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> '''
+components.html(song_html, width=768, height=432, scrolling=False)
